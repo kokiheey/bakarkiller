9feb1556b27e38f1a3f5cd650910104735074401
koki <kpasic22@rg.edu.rs>
2023-12-03 17:16:46 +0100

    Add movement

--- player.gd
+++ player.gd
@@ -1,15 +1,26 @@
-extends Node3D
+extends CharacterBody3D
 @onready var camera = $cameracomponent
 var bakar = preload("res://bakar.tscn")
 var rot_x = 0
 var rot_y = 0
-var speed = 10
+var speed = 20
+const move_speed = 5
 @export var sens = 0.005
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
+func physics_process(delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (Vector3(input_dir.x, 0, input_dir.y) * camera.transform.basis).normalized() #z == foward
+	if direction:
+		velocity.x = direction.x * move_speed
+		velocity.y = direction.y * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
+	move_and_slide()
+	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rot_x += -event.relative.x * sens
@@ -21,7 +32,7 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("click"):
 			var ciga = bakar.instantiate()
-			ciga.scale = Vector3(0.02, 0.02, 0.02)
+			ciga.scale = Vector3(0.01, 0.01, 0.01)
 			ciga.transform.basis = camera.transform.basis
 			ciga.linear_velocity = -camera.transform.basis.z * speed
 			add_child(ciga)
